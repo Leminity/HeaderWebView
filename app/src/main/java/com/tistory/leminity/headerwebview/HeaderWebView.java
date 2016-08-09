fabf2691badfbe9dd3b896f6d8898674c1763dc4
leminity@gmail.com <zmfltkdhfm103>
2016-08-09 18:17:48 +0900

    # impl  - implement HeaderWebView.  - add Sample Code

--- app/src/main/java/com/tistory/leminity/headerwebview/HeaderWebView.java
+++ app/src/main/java/com/tistory/leminity/headerwebview/HeaderWebView.java
@@ -2,29 +2,145 @@ package com.tistory.leminity.headerwebview;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 
 /**
  * Created by User on 2016-07-27.
  */
 public class HeaderWebView extends WebView {
+
+    private static String   mJsFunctionChangeTop = "javascript:saferChangePostionTop(\"#0\")";
+
+    private View            mViewHeader;
+    private LayoutParams    mLayoutParamsHeader;
+    private float           mDensity;
+
     public HeaderWebView(Context context) {
         super(context);
+        init();
     }
 
     public HeaderWebView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public HeaderWebView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public HeaderWebView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        mDensity        = getResources().getDisplayMetrics().density;
+    }
+
+    public void setHeaderView(View v) {
+        if(mViewHeader == v) return;
+        if(mViewHeader != null) {
+            removeView(mViewHeader);
+        }
+
+        if(null != v) {
+            mLayoutParamsHeader = new LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT, 0, 0);
+            addView(v, mLayoutParamsHeader);
+        }
+        mViewHeader = v;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.save();
+        if(mViewHeader != null) {
+            mViewHeader.setTranslationX(getScrollX());
+        }
+        super.onDraw(canvas);
+        canvas.restore();
+    }
+
+    @Override
+    protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
+        if(getHeaderVisibleHeight() > 0)
+            changeDomTopPosition(getHeaderHeight());
+        return super.drawChild(canvas, child, drawingTime);
+    }
+
+    private int getHeaderHeight() {
+        if(mViewHeader != null)
+            return (int)(mViewHeader.getHeight() / mDensity);
+        return 0;
+    }
+
+    private int getHeaderVisibleHeight() {
+        return Math.max(getHeaderHeight() - getScrollY(), 0);
+    }
+
+    private void changeDomTopPosition(int height) {
+        loadUrl(mJsFunctionChangeTop.replace("#0", String.valueOf(height)));
+    }
+
+    /******************************************************************************************************************
+     * 터치 이벤트 핸들링
+     *
+     * 헤더뷰 내 스크롤 이벤트를 가진 뷰가 존재 시(ex:EditText) 웹뷰가 스크롤되지 않는다.
+     * 이는 OS 터치 핸들링 구조 상 정상 동작이지만, 해당 뷰에서는 헤더 영역의 height가 높을 경우 웹뷰를 스크롤하는데
+     * 어려움을 유발한다.
+     *
+     * 위 사유로 웹뷰 스크롤 처리와 별개로 헤더뷰에 터치 이벤트를 전달하도록 한다.
+     ******************************************************************************************************************/
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if(mViewHeader != null)
+            return true;
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean result = super.onTouchEvent(event);
+        if(mViewHeader != null) {
+            dispatchTouchWhenMatchedPosition(mViewHeader, event);
+        }
+        return result;
+    }
+
+    private void dispatchTouchWhenMatchedPosition(View headerView, MotionEvent ev) {
+        int visibleHeaderHeight = getHeaderVisibleHeight();
+        int eventYDp     = (int)(ev.getY() / mDensity);
+        if(eventYDp <= visibleHeaderHeight) {
+            headerView.dispatchTouchEvent(ev);
+        }
+    }
+
+    /******************************************************************************************************************
+     * 키 이벤트 핸들링
+     *
+     * 웹뷰에서 키보드 이벤트를 처리하면서 EditText까지 이벤트가 전달되지 않는 문제가 있음.
+     * 키 이벤트 발생 시, 헤더뷰로 수동 전달한다.
+     ******************************************************************************************************************/
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if(mViewHeader != null) {
+            mViewHeader.dispatchKeyEvent(event);
+            return false;
+        }
+        return super.dispatchKeyEvent(event);
     }
 
 }
--- app/src/main/java/com/tistory/leminity/headerwebview/MainActivity.java
+++ app/src/main/java/com/tistory/leminity/headerwebview/MainActivity.java
@@ -1,13 +1,128 @@
 package com.tistory.leminity.headerwebview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.AssetManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+
+//    final String SAMPLE_URL = "http://app.directreader.co.kr/mem";
+
+    private HeaderWebView mMailSaferWebView;
+    private TextView      mTextViewChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        mMailSaferWebView = (HeaderWebView) findViewById(R.id.mailsaferWebView);
+
+        addHeaderView(mMailSaferWebView, createHeaderView());
+        WebSettings webSettings = mMailSaferWebView.getSettings();
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setJavaScriptEnabled(true);
+        loadAssetsHtml(mMailSaferWebView);
+    }
+
+    private View createHeaderView() {
+        View header = LayoutInflater.from(getBaseContext()).inflate(R.layout.view_header, mMailSaferWebView, false);
+
+        mTextViewChild = (TextView)header.findViewById(R.id.textViewChild);
+        Button button = (Button)header.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String LABEL_EXPAND = "Expand";
+                final String LABEL_HIDE   = "Hide";
+
+                Button clickedBtn = (Button) v;
+                int previousVisibility = mTextViewChild.getVisibility();
+
+                switch (previousVisibility) {
+                    case View.VISIBLE :
+                        mTextViewChild.setVisibility(View.GONE);
+                        clickedBtn.setText(LABEL_EXPAND);
+                        break;
+
+                    case View.GONE :
+                        mTextViewChild.setVisibility(View.VISIBLE);
+                        clickedBtn.setText(LABEL_HIDE);
+                        break;
+                }
+            }
+        });
+
+        return header;
+    }
+
+    private void addHeaderView(HeaderWebView dstWebView, View headerView) {
+        dstWebView.setHeaderView(headerView);
+    }
+
+    private void loadAssetsHtml(final WebView webView) {
+        Runnable loadHtmlRunnable = new Runnable() {
+            @Override
+            public void run() {
+                AssetManager assetManager = getBaseContext().getAssets();
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+                try {
+                    InputStream in = assetManager.open("sample.html");
+                    readStream(in, bos);
+                    String htmlContents = new String(bos.toByteArray());
+
+                    loadHtml(webView, htmlContents);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return;
+                }
+
+            }
+        };
+
+        Executor executor = Executors.newSingleThreadExecutor();
+        executor.execute(loadHtmlRunnable);
+    }
+
+    private void loadHtml(final WebView webView, final String htmlContents) {
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                String mimeType = (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN ? "text/html; charset=UTF-8" : "text/html");
+                String charSet =  (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN ? null : "UTF-8");
+                webView.loadDataWithBaseURL(null, htmlContents, mimeType, charSet, null);
+            }
+        });
+    }
+
+    private void readStream(InputStream in, OutputStream out) throws IOException{
+        int readCnt = 0;
+        byte[] buf = new byte[1024*4];
+
+        while((readCnt = in.read(buf)) != -1) {
+            out.write(buf, 0, readCnt);
+        }
     }
 }
